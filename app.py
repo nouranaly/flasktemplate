c62575d56e56a0e906e8f0aee2a65b2e2f091945
nouran <naly791@gmail.com>
2021-04-20 20:57:12 +0000

    first save

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import get_breakfast_rating
 
 
 # -- Initialization section --
@@ -13,4 +14,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/secret')
+def secret():
+    return"you found the secret page"
+
+@app.route('/results', methods=["GET","POST"])
+def results():
+    if request.method =="POST":
+        print(request.form["breakfast"])
+        user_breakfast = request.form["breakfast"]
+        user_nickname=request.form["nickname"]
+        breakfast_rating = get_breakfast_rating(user_breakfast)
+        return render_template("results.html",user_breakfast=user_breakfast, user_nickname=user_nickname, breakfast_rating=breakfast_rating )
+    else:
+        return "error"
---
+++ model.py
@@ -0,0 +1,5 @@
+def get_breakfast_rating(food):
+    if food.lower() == "waffles":
+        return "thats an amazing breakfast"
+    else:
+        return "that sounds cool"
